3a8a8738b004f4ba9218ca11d463b24e54091ff0
A2m <102222819+A2mTech@users.noreply.github.com>
2024-01-21 19:56:24 +0100

    feat: brand logo component

---
+++ src/components/Brand/index.tsx
@@ -0,0 +1,7 @@
+export default function Brand() {
+    return (
+        <div className="flex items-center justify-center">
+            <img src="/logo.png" alt="logo" className="h-10"/>
+        </div>
+    )
+}
