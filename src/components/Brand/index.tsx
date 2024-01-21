7ab971ba70d32847a03b0b19ab42eecfc86f5067
A2m <102222819+A2mTech@users.noreply.github.com>
2024-01-21 21:40:23 +0100

    fix: logo dimensions

--- src/components/Brand/index.tsx
+++ src/components/Brand/index.tsx
@@ -1,7 +1,7 @@
 export default function Brand() {
     return (
-        <div className="flex items-center justify-center">
-            <img src="/logo.png" alt="logo" className="h-10"/>
+        <div className="flex items-center justify-center flex-shrink-0" style={{ minWidth: '15.5rem' }}>
+            <img src="/logo.png" alt="logo" className="h-10 w-50 object-contain"/>
         </div>
     )
 }
