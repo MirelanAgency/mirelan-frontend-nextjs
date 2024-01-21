f16daf22259c7c9d44ba06d0d965e5fa2cf7c5cb
A2m <102222819+A2mTech@users.noreply.github.com>
2024-01-21 21:40:12 +0100

    feat: HeroSection base

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+import HeroSection from "@/containers/home-page/hero-section";
 
-    </main>
-  );
+export default function Home() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between">
+            <HeroSection />
+        </main>
+    );
 }
---
+++ src/containers/home-page/hero-section/index.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link"
+import React, {FC} from "react";
+
+interface HeroSectionProps {}
+
+const HeroSection: FC<HeroSectionProps> = ({}) => {
+    return(
+        <section>
+            <p className="text-2xl">
+                ddqzdqzd
+            </p>
+        </section>
+    )
+}
+
+export default HeroSection;
