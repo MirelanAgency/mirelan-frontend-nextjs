14c745bd79ee17d255b769e65ae90f1cfe2f921b
A2m <102222819+A2mTech@users.noreply.github.com>
2024-01-22 12:55:45 +0000

    refactor: change navbar with new components lib

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from "next";
 import {Outfit} from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { StickyNavbar } from "@/components/NavbarTest";
 
 const inter = Outfit({subsets: ["latin"]});
 
@@ -18,7 +19,9 @@ export default function RootLayout({
     return (
         <html lang="en">
         <body className={inter.className}>
-        <Navbar/>
+        {/* <Navbar/> */}
+
+        <StickyNavbar />
 
         {children}
 
